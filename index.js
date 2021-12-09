82d9cb9fcf2e4bc953b7f12b420e0f1e47b54892
emad hashem <eali32028@gmail.com>
2021-12-09 21:12:20 +0200

    save heoku staff

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+
+
+
+
+const PORT = process.env.PORT || 5050
+
+app.listen(PORT, () => {
+    console.log(`app is ruuning on ${PORT}`)
+
+})
